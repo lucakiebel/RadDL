804185018f5b875c0041d70081e70cfd42b9bb68
Luca Kiebel <github@luca-kiebel.de>
2018-05-25 23:14:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ $(document).ready(() => {
 		let a = document.createElement("A");
 		a.download = true;
 		if ($input.val() && $input.val().substring($input.val().length-4, $input.val()-1).toLowerCase() === "gpx") {
+			console.log("Input fits");
 			a.src = $input.val();
+			console.log("Input Value: ", $input.val());
 			a.click();
 		} 
 	});
