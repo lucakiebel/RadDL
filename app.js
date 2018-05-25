3c47b9eee9f5422bb42841af98935b8a5ed14cd4
Luca Kiebel <github@luca-kiebel.de>
2018-05-26 00:05:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,14 +7,11 @@ $(document).ready(() => {
 		a.download = "FahrradRoute.gpx";
 		if ($input.val() && $input.val().split(".")[$input.val().split(".").length-1] === "gpx") {
 			console.log("Input fits");
-			$.get($input.val(), data => {
-				console.log(data);
-				a.href = $input.val();
-				console.log("Input Value: ", $input.val());
-				document.body.appendChild(a);
-				//a.click();
-			});
-			
+			a.href = "http://sk-cdn.net/websites/rad.luca.lk/api.php?url="+$input.val();
+			document.body.appendChild(a);
+			a.click();
+			document.body.removeChild(a);
+			delete a;
 		} 
 	});
 });
